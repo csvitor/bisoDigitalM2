5ee8b6488242ed65614441f4bb6c696a8d51542f
Vitor Costa <developer.vitorcosta@gmail.com>
2025-09-01 08:28:13 -0300

    Restore redirect to admin on root URL

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-   // return redirect()->to('admin');
-   echo 'Biso Magento2' ; // temporary
+    return redirect()->to('admin');
 });
