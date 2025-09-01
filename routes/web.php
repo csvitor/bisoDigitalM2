c2e979ed0866db9e99a88bf00a951b3b3ad58d75
Vitor Costa <developer.vitorcosta@gmail.com>
2025-09-01 08:21:10 -0300

    Temporarily display 'Biso Magento2' on root URL instead of redirecting to admin

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->to('admin');
+   // return redirect()->to('admin');
+   echo 'Biso Magento2' ; // temporary
 });
